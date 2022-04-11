fa74e20ce780c743307900e793f4480b10c8bb37
Antonio Araujo <paja270265@gmail.com>
2022-04-11 09:13:09 -0300

    JobExemplo

---
+++ JobExemplo/src/main/java/Araujo/JobExemplo/JobExemploApplication.java
@@ -0,0 +1,13 @@
+package Araujo.JobExemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobExemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobExemploApplication.class, args);
+	}
+
+}
---
+++ JobExemplo/src/test/java/Araujo/JobExemplo/JobExemploApplicationTests.java
@@ -0,0 +1,13 @@
+package Araujo.JobExemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobExemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
